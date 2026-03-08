b306a74fa7520ac8c964f1ed5de1a5f568531dfd
Chhun Menghour <chhunmenghour@Hxur-5.local>
2026-03-08 21:02:26 +0700

    Fix db connection and add nixpacks config

--- db.php
+++ db.php
@@ -1,10 +1,13 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "businessdb";
-    $conn = new mysqli($servername, $username, $password, $dbname);
-if($conn -> connect_error){
-    die("Connection failed " . $conn -> connect_error);
-}
+    $servername = getenv('MYSQLHOST');
+    $username = getenv('MYSQLUSER');
+    $password = getenv('MYSQLPASSWORD');
+    $dbname = getenv('MYSQL_DATABASE');
+    $port = getenv('MYSQLPORT');
+
+    $conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+    if($conn->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }
 ?>
